f544847f7ef299506784842837e6d9d26c7256b7
Quanghihicoder <hoangquang2508@gmail.com>
2025-06-08 15:17:58 +1000

    delete

--- workyard/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import Projects from './ProjectReducer';
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-  projects: Projects
-});
-
-
-export default rootReducer;
