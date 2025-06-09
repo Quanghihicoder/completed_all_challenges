a0c1b2f1ef3a9cb6afb12a911ad0a5137bff3781
Quanghihicoder <hoangquang2508@gmail.com>
2025-06-09 11:24:05 +1000

    cleanup

--- qa-challenge/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  verbose: true,
-  bail: true,
-  collectCoverage: true,
-  preset: "ts-jest",
-  testRegex: "(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$",
-};
--- qa-challenge/src/types.ts
+++
@@ -1,2 +0,0 @@
-export type TileDescriptor = [x: string, y: string, zoom: string];
-export type TileSet = TileDescriptor[];
